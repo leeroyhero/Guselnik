d41cc890c738ee9f04a1d79291a5200a2f71a1b8
leero <home>
2019-11-20 21:41:31 +0300

    Initial commit

---
+++ app/src/main/java/ru/bogdanov/guselnik/CraftActivity.kt
@@ -0,0 +1,12 @@
+package ru.bogdanov.guselnik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CraftActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_craft)
+    }
+}
